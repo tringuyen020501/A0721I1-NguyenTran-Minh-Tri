8d589b74197914fb9998b7756deb96e9de1126ff
TMTN <tringuyen2501@gmail.com>
2021-10-01 18:58:27 +0700

    thuchanh

---
+++ Module 2/src/bai1_Introduction_to_Java/thuc_hanh/TimeServer.java
@@ -0,0 +1,10 @@
+package bai1_Introduction_to_Java.thuc_hanh;
+
+import java.util.Date;
+
+public class TimeServer {
+    public static void main(String[] args) {
+        Date now= new Date();
+        System.out.println("now is" + now);
+    }
+}
