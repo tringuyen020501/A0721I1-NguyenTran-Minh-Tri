7d9a18399e0133b5c4ab7b372dc8ca84236dbca9
TMTN <tringuyen2501@gmail.com>
2021-10-04 18:47:03 +0700

    bai_tap

--- Module 2/src/bai2_vong_lap_trong_java/bai_tap/HienThi20SoNguyenTo.java
+++ Module 2/src/bai2_vong_lap_trong_java/bai_tap/HienThi20SoNguyenTo.java
@@ -3,28 +3,27 @@ package bai2_vong_lap_trong_java.bai_tap;
 public class HienThi20SoNguyenTo {
     public static void main(String[] args) {
         int count = 0;
-        int number = 2;
-        String result = "";
-        while (count < 20) {
-            boolean check = true;
-            if (number < 2) {
-                check = false;
-                for (int i = 2; i < number; i++) {
-                    if (number % i == 0) {
-                        check = false;
-                        break;
-                    }
+        boolean check = true;
+        int num = 2;
+        System.out.println("20 số nguyên tố đầu tiên là:");
+        for (int i = 2; i < 23; ) {
+            for (int j = 2; j <= Math.sqrt(num); j++) {
+                if (num % j == 0) {
+                    check = false;
+                    break;
                 }
             }
-            if (check) {
-                result += number+",";
-                count++;
+                if (check) {
+                    System.out.println(num);
+                    i++;
+                    count++;
+                }
+                check = true;
+                num++;
             }
-            number++;
         }
-        System.out.println(result);
     }
-}
+
 
 
 
